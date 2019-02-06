c4b51e9c5502932509fe909d57fa97851c19aece
HikkaV <45285898+HikkaV@users.noreply.github.com>
2019-02-06 20:50:22 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,23 @@
+from threading import Thread
+import logging
+from Server import Server
+
+if __name__ == '__main__':
+    logger = logging.getLogger("Server")
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler("Server.log")
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(threadName)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    host = ''
+    port = 8889
+    server = Server(host,port)
+    logger.info('The program started')
+    logger.info('Waiting for connection')
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=server.accept_incoming_connections)
+    ACCEPT_THREAD.start()  # Starts the infinite loop.
+    ACCEPT_THREAD.join()
+
+   
+   
---
+++ Settings.py
@@ -0,0 +1,4 @@
+path_to_model = '/home/hikkav/environments/my_env/validCNNS/the _best_MODEL.h5'
+path_to_labels = '/home/hikkav/environments/my_env/labels_for_server.json'
+path_to_pics = "/home/hikkav/PycharmProjects/ServerWithNN/pics/"
+commands = ['quit', 'predict']
