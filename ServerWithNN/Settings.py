72d13f771891d69014ca47304b32c61f3166d37e
HikkaV <45285898+HikkaV@users.noreply.github.com>
2019-02-11 21:27:40 +0200

    Delete Settings.py

--- ServerWithNN/Settings.py
+++
@@ -1,7 +0,0 @@
-path_to_model = '/home/hikkav/environments/my_env/validCNNS/the _best_MODEL.h5'
-path_to_labels = '/home/hikkav/environments/my_env/labels_for_server.json'
-path_to_save_imgs = "/home/hikkav/PycharmProjects/ServerWithNN/pics/"
-path_to_pic = '/home/hikkav/PycharmProjects/ServerWithNN/'
-commands = ['quit', 'predict']
-port_ = 8889
-host_ = ''
